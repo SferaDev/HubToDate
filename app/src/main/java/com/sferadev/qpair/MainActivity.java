a7181438e395f0d00268747dd14af800896c46d1
SferaDev <sferadev@gmail.com>
2014-10-30 19:30:48 +0100

    Initial commit

---
+++ app/src/main/java/com/sferadev/qpair/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sferadev.qpair;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected int getLayoutResource() {
+        return R.layout.activity_main;
+    }
+
+}
