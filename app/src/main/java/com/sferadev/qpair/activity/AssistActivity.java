d3a664e8676b82c2bc6b9861d411030dc862251b
SferaDev <sferadev@gmail.com>
2014-11-27 19:08:24 +0100

    Simplify Assist once again

--- app/src/main/java/com/sferadev/qpair/activity/AssistActivity.java
+++ app/src/main/java/com/sferadev/qpair/activity/AssistActivity.java
@@ -3,24 +3,14 @@ package com.sferadev.qpair.activity;
 import android.app.Activity;
 import android.os.Bundle;
 
-import com.sferadev.qpair.R;
-
-import static com.sferadev.qpair.App.getContext;
-import static com.sferadev.qpair.utils.QPairUtils.isConnected;
-import static com.sferadev.qpair.utils.QPairUtils.isQPairOn;
 import static com.sferadev.qpair.utils.Utils.createAssistDialog;
-import static com.sferadev.qpair.utils.Utils.openActivity;
 
 public class AssistActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(isQPairOn() && isConnected()) {
-            createAssistDialog();
-        } else {
-            openActivity(getContext().getResources().getString(R.string.qpair_package));
-        }
+        createAssistDialog();
         finish();
     }
 
